33b67edaf486877bf044b157c605d443015dbaf3
Livia <liviaflore@uol.com.br>
2022-08-25 14:07:36 -0300

    criação da Home (css e tsx)

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import'./Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src='https://i.imgur.com/ZnDBeln.jpeg' alt='Imagem de Jupiter capturada pelo telescópio Webb da NASA' className="img"/>
+        </>
+    );
+}
+
+export default Home;
