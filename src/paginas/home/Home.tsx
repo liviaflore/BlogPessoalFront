889d64a27529d31dea66c2b1f92718ffa255c54c
Livia <liviaflore@uol.com.br>
2022-08-29 14:56:17 -0300

    customização da Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import {Box, Paper, Button} from '@mui/material';
+import {Grid, Box, Paper, Button, Typography} from '@mui/material';
 import'./Home.css';
 
 function Home(){
     return(
         <>
-     <Paper>
-      <Box p={2}>
-        <Box display="flex" justifyContent="center">
-          <h1>Título</h1>
-        </Box>
-        <img src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-5mb.jpg" alt="Imagem do espaço capturada pela NASA, por meio do James Webb Space Telescope" style={{width: "100%", height: "100%"}}/>
-      <Box display="flex" justifyContent="center" p={2}>
-        <Button variant="contained" color="primary">Texto 1</Button>
-        <Button variant="contained" color="secondary">Texto 2</Button>
-        </Box>
-      </Box>
-     </Paper>
+    
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/main_image_star-forming_region_carina_nircam_final-5mb.jpg" alt="Imagem do espaço capturada pela NASA, por meio do James Webb Space Telescope" 
+                    width="100%" height="100%" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
