6288e800598a26f01182ecb9f7121a1d66d9a557
Livia <liviaflore@uol.com.br>
2022-08-30 10:20:54 -0300

    implementação do React-Router-Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import {Grid} from '@mui/material';
 import Home from './paginas/home/Home';
+
+import {Grid} from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
 import './App.css';
 
 
 
 function App() {
   return(
-    <>
+    <Router>
     <Navbar />
-    <Home />
+    <Routes> 
+    {/*<Route path="/" element={<Login />} /> 
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
+   {/* <Route path="/cadastro" element={<CadastroUsuario />} /> // Ativar depois de criar as págs Login e Cadastro*/}
+
+    <Route path="/" element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    </Routes>
     <Footer />
-    </>
+    </Router>
+    
   );
 }
 
