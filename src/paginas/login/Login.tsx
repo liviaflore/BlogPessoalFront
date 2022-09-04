468afd0ba440dc921e1b9a86bd1b0346a441f5ef
Livia <liviaflore@uol.com.br>
2022-09-04 15:49:46 -0300

    ajustes na pág de Login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -8,26 +8,26 @@ import './Login.css';
 function Login() {
 
     return (
-        <Grid container className='back'>
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
         <Grid alignItems='center' xs={6} className='login'>
             <Box paddingX={20}>
                <form>
-                <Typography className='titulologin'> Entrar </Typography>
-                <TextField id='Username' label='Username' name='Username'/>
-                <TextField id='Password' label='Password' name='Password' />
+                <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='titulologin'> Olá! </Typography>
+                <TextField id='Usuario' label='Usuario' name='Usuario' variant='outlined' margin='normal' fullWidth/>
+                <TextField id='Senha' label='Senha' name='Senha' variant='outlined' margin='normal' fullWidth/>
                   <Box marginTop={2} textAlign='center'>
                     <Link to='/home' className='text-decorator-none'>
                       <Button type='submit' variant='contained' color='primary'>
-                        Logar
+                        Entrar
                       </Button>
                     </Link>
                 </Box>
                </form>
-               <Box>
+               <Box display='flex' justifyContent='center' marginTop={2}>
                 <Box>
-                 <Typography variant='subtitle1' gutterBottom color='textPrimary' align='center'> Não tem uma conta? </Typography>
+                 <Typography variant='subtitle1' gutterBottom color='textPrimary' align='center'> Não tem uma conta?</Typography>
                 </Box>
-                 <Typography variant='subtitle1' gutterBottom color='textPrimary' align='center' className='textos1'> Cadastre-se! </Typography>
+                 <Typography variant='subtitle1' gutterBottom color='textPrimary' align='center' className='textos1'>  Cadastre-se! </Typography>
                </Box>
 
             </Box>
@@ -37,40 +37,6 @@ function Login() {
         <Grid xs={6} className='imagem1'>
 
         </Grid>
-
-        <Grid alignItems='center' xs={6} className='signup'>
-            <Box paddingX={20}>
-               <form>
-                <Typography className='titulologin'> Cadastrar </Typography>
-                <TextField id='Username' label='Username' name='Username'/>
-                <TextField id='Password' label='Password' name='Password' />
-                  <Box marginTop={2} textAlign='center'>
-                    <Link to='/home' className='text-decorator-none'>
-                      <Button type='submit' variant='contained' color='primary'>
-                        Logar
-                      </Button>
-                    </Link>
-                </Box>
-               </form>
-               <Box>
-                <Box>
-                 <Typography variant='subtitle1' gutterBottom color='textPrimary' align='center'> Não tem uma conta? </Typography>
-                </Box>
-                 <Typography variant='subtitle1' gutterBottom color='textPrimary' align='center' className='textos1'> Cadastre-se! </Typography>
-               </Box>
-
-            </Box>
-
-        </Grid>
-
-        <Grid xs={6} className='imagem2'>
-
-        </Grid>
-
-
-
-
-
 
         </Grid>
 
