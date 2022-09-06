1b12716c5f0b3688ef49f579d10365a929cfe857
Livia <liviaflore@uol.com.br>
2022-09-05 22:32:08 -0300

    adição das routes temas e posts

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+
 
 import {Grid} from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -26,6 +29,8 @@ function App() {
        <Route path="/login" element={<Login />} />
        <Route path="/home" element={<Home />} />
        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+       <Route path="/temas" element={<ListaTema />} />
+       <Route path="/posts" element={<ListaPostagem />} />
 
 
    {/* <Route path="*" element={<NotFound />} /> // Ativar depois de criar a pág de Not Found*/}
